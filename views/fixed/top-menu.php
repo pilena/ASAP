1b9333f095336d3094dfcbc166b2a46926b5ac37
pilena24 <lenkazivkovic24@gmail.com>
2019-10-24 21:39:23 +0200

    Php -> Views

---
+++ views/fixed/top-menu.php
@@ -0,0 +1,25 @@
+<div class="container">
+				<div class="row">
+					<div class="col-xs-12 text-center logo-wrap">
+						<div id="fh5co-logo"> <a title='ASAP' href='index.php'> <img src='images/logo/logo-glic.png' alt='ASAP logo'></a> 
+						<h1> All Star Alience  </h1>
+					</div>
+						
+						
+					</div>
+					<div class="col-xs-12 text-center menu-1 menu-wrap">
+						<ul>
+							<li><a title='Početna stranica' href="index.php">Početna</a></li>
+							<li><a title='Stranica o nama' href="o-nama.php">O nama</a></li>
+							<li><a title='Stranica o dogadjajima'  href="dogadjaji.php">Događaji</a></li>
+							<li>
+								<a title='Stranica o izvođačima' href="izvodjaci.php">Izvođači</a>
+							</li>
+							<li><a title='Redirekcija na MixCloud' target='_blank' href="https://www.mixcloud.com/asappancevo/">Muzika</a></li>
+							<li><a title='Galerija slika' href="galerija.php">Galerija</a></li>
+							<li><a  title='Kontakt stranica' href="kontakt.php">Kontakt</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
