0fe2257c00ea906b6cdec448143a6168d74ccd3b
pilena24 <lenkazivkovic24@gmail.com>
2019-12-02 20:48:51 +0100

    Dodavanje pocetne, ne pitaj me

---
+++ pocetna.php
@@ -0,0 +1,208 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-NZ8Q3X6');
+	</script>
+	<!-- End Google Tag Manager -->
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Najbolje žurke elektronske muzike | ASAP </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="ASAP je ekipa dobro poznatih zaljubljenika u elektronsku muziku koja godinama organizuje istoimene događaje u Pančevu." />
+	<meta name="keywords" content="ASAP, elektronska muzika, dogadjaj, muzika, Pancevo, zurke" />
+	<meta name="author" content="Lenka živković" />
+
+	<link rel="shortcut icon" href="images/logo/logo-title.png">
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,600i,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet"> -->
+
+	<!-- FONT  -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<link href="lib/venobox/venobox.css" rel="stylesheet">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+
+	<link href="lib/venobox/venobox.css" rel="stylesheet">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	<!-- Primary Meta Tags -->
+	<title>ASAP presents Space Motion</title>
+	<meta name="title" content="ASAP presents Space Motion">
+	<meta name="description" content="U subotu, 19. oktobra, ASAP se vraća u Apolo!  Naš prvi gost sezone je @space_motion_, novosadski DJ i producent koji preko 16 godina oduševljava svojim progressive, tech i afro house zvukom.">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://metatags.io/">
+	<meta property="og:title" content="ASAP presents Space Motion">
+	<meta property="og:description" content="U subotu, 19. oktobra, ASAP se vraća u Apolo!  Naš prvi gost sezone je @space_motion_, novosadski DJ i producent koji preko 16 godina oduševljava svojim progressive, tech i afro house zvukom.">
+	<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+</head>
+
+<body>
+
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ8Q3X6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+		<nav class="fh5co-nav" role="navigation">
+			<!-- <div class="top-menu"> -->
+			<?php
+			include "views/fixed/top-menu.php";
+			?>
+			<!-- </div> -->
+		</nav>
+
+		<div id="boxes">
+			<div id="dialog" class="window">
+				<div id="san">
+					<a href="#" title='ASAP presents Andrew Meller' class="close agree"><img src="images/events/error.png" width="25" style="float:right; margin-right: -25px; margin-top: -20px;"></a>
+					<img alt='ASAP presents Andrew Meller' id='Space' src="images/events/andrew.jpg" width="450">
+				</div>
+			</div>
+			<div style="width: 2478px; font-size: 32pt; color:white; height: 1202px; display: none; opacity: 0.4;" id="mask"></div>
+		</div>
+
+
+		<section id="intro">
+			<div class="intro-container wow fadeIn">
+				<h1 class="mb-4 pb-0">All stars<br><span>alliance</span> Pančevo</h1>
+
+				<a href="https://www.youtube.com/watch?v=nKh7YcZOOD0&feature=youtu.be&fbclid=IwAR1pTVbIRx365lB3SwO-pUBDpq_rEznSVnsSkN4-dDGh0DwKDSRnCM80pMI" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+
+				<script type='text/javascript'>
+					$('.venobox').venobox({
+						bgcolor: '',
+						overlayColor: 'rgba(6, 12, 34, 0.85)',
+						closeBackground: '',
+						closeColor: '#fff'
+					});
+				</script>
+
+			</div>
+		</section>
+
+
+
+		<div id="fh5co-about" class="fh5co-section-index">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-4 img-wrap animate-box" data-animate-effect="fadeInLeft">
+						<img src="images/about1.jpg" alt="Artist photo" />
+					</div>
+					<div class="col-md-6 col-md-1 animate-box">
+						<div class="section-heading">
+							<h2>O nama</h2>
+							<p> ASAP je ekipa dobro poznatih zaljubljenika u elektronsku muziku koja godinama organizuje istoimene događaje u Pančevu. Nastala s idejom da se lokalnoj publici približi ono najbolje što Pančevo ima da ponudi, ASAP je prerastao u ozbiljan serijal žurki.</p>
+							<p> Sveže ideje i sve veći bukinzi, s željom da se osveži i osnaži klupska scena Pančeva, podigli su ovu priču na mnogo ozbiljniji nivo, pa su na ASAP žurkama do sada mogla da se čuju najperspektivnija imena elektronske muzike u Srbiji.</p>
+							<p><a href="about.php" title='Redirekcija na stranicu O nama' class="btn btn-primary btn-outline">Saznajte više o nama</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div id="fh5co-slider" class="fh5co-section-index animate-box">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 animate-box">
+						<div class="fh5co-heading">
+							<h2>Naš sledeći event </h2>
+						</div>
+					</div>
+					<div class="col-md-6 col-md-1 animate-box">
+						<aside id="fh5co-slider-wrwap">
+							<div class="flexslider">
+								<ul class="slides">
+									<li style="background-image: url(images/events/andrew.jpg) ;">
+										<div class="overlay-gradient"></div>
+										<div class="container-fluid">
+											<div class="row">
+												<div class="col-md-12 col-md-offset-0 col-md-pull-10 slider-text slider-text-bg">
+													<div class="slider-text-inner">
+														<div class="desc">
+															<h2><a title='Facebook event | ASAP presents Andrew Meller' href='https://www.facebook.com/events/783768405401705/'> ASAP presents Andrew Meller</a></h2>
+															<p> Domaći DJ/producent @andrew.meller vraća se u Pančevo 21. decembra! 🤩💥🚀 Slušamo ga u dvorani Apolo uz gosta iz Beograda @fisherfilip, našeg rezidenta @djrescobar i mladu nadu @istina_izazov!</p>
+															<p><a href="dogadjaji.php" class="btn btn-primary btn-outline" title='Redirekcija na stranicu Dogadjaju'>Svi događaji</a></p>
+															<p class='tooltip'> <a class="btn btn-primary btn-outline" href='https://www.facebook.com/events/783768405401705/' title='Facebook event |ASAP presents Andrew Meller'>Pogledaj event</a>
+																<!-- <span class="tooltiptext">Karte se kupuju na ulazu </span> -->
+															</p>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</li>
+
+								</ul>
+							</div>
+						</aside>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php
+		include "views/fixed/book.php";
+		?>
+
+		<footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
+			<?php
+			include "views/fixed/footer.php";
+			?>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+	</div>
+
+	<?php
+	include "views/fixed/top-js-includes.php";
+	?>
+
+
+</body>
+
+</html>
